b4bb1a21e3a225e3c56d22b7f906993cdcecc86b
Francesco <fpfrancescopalma7@gmail.com>
2016-03-03 09:54:46 +0000

    Update

--- models/shoos.rb
+++ models/shoos.rb
@@ -12,17 +12,17 @@ class Shoos
   end
 
   def save
-    db = PG.connect( { dbname: "shoos", host: "localhost"} )
+    db = PG.connect({dbname: "shoos", host: "localhost"})
     sql = "INSERT INTO orders (name, address, quantity, size)
       VALUES ('#{@name}', '#{@address}', #{@quantity}, #{@size});"
-    db.exec( sql )
+    db.exec(sql)
     db.close
   end
   
   def self.all
-    db = PG.connect( { dbname: "shoos", host: "localhost"} )
+    db = PG.connect({dbname: "shoos", host: "localhost"})
     sql = "SELECT * FROM orders;"
-    result = db.exec( sql ).map { |shoos| Shoos.new(shoos) }
+    result = db.exec(sql).map { |shoos| Shoos.new(shoos) }
     db.close
     return result
   end
